f26062234eee9a14be1e93ef528927d33310e6e5
Jonathan Boler <galatians@gmail.com>
2024-04-18 11:35:18 -0500

    add rswag

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem "sprockets-rails"
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
+gem 'rswag-api'
+gem 'rswag-ui'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,13 @@ module RswagCsp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.    
+    config.api_only = false
+
+    # Add CSP middleware manually when api_only = true
+    # config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware
   end
 end
